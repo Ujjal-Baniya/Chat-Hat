b372e34a624025441d89b5d84b8edf3c387cad19
Ujjal-Baniya <63017383+Ujjal-Baniya@users.noreply.github.com>
2020-08-28 22:32:54 +0530

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,51 @@
 const socket = io();
 const chatForm = document.getElementById("chat-form");
+const chatMessages = document.querySelector(".chat-messages")
 
 
+// get username from url
+const {username, room} = Qs.parse(location.search, {
+    ignoreQueryPrefix: true 
+});
+
+
+// Join Room
+socket.emit("joinroom", {username, room});
+
+// Message from server
 socket.on("message", message=>{
     console.group(message);
+    outputMessage(message);
+
+    // scroll down
+    chatMessages.scrollTop = chatMessages.scrollHeight
 })
 
 
+// submit
 chatForm.addEventListener('submit', (e)=>{
     e.preventDefault();
+
     // Get Message from user
     const msg = e.target.elements.msg.value;
 
     // send message to server
     socket.emit("chatMessage", msg)
+
+
+    // clear input
+    e.target.elements.msg.value = ""
+    e.target.elements.msg.foucs();    
 })
+
+// Output Message to DOM
+
+function outputMessage(message){
+    const div = document.createElement('div');
+    div.classList.add("message");
+    div.innerHTML =`<p class="meta">${message.userName}<span>${message.time}</span></p>
+    <p class="text">
+        ${message.text}
+    </p>`;
+    document.querySelector(".chat-messages").appendChild(div)
+}

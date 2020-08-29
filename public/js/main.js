8276f05cc63af5be2c70c3af2b808a9c758d3b2a
Ujjal-Baniya <63017383+Ujjal-Baniya@users.noreply.github.com>
2020-08-29 22:20:25 +0530

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,51 +1,76 @@
+const chatForm = document.getElementById('chat-form');
+const chatMessages = document.querySelector('.chat-messages');
+const roomName = document.getElementById('room-name');
+const userList = document.getElementById('users');
+
+// Get username and room from URL
+var { username, room } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true
+});
 const socket = io();
-const chatForm = document.getElementById("chat-form");
-const chatMessages = document.querySelector(".chat-messages")
 
+// Join chatroom
+if (room=="Public"){
+  socket.emit('joinRoom', { username, room });
+}
+else{
+  const { username, RoomID } = Qs.parse(location.search, {
+    ignoreQueryPrefix: true
+  });
+  room = RoomID
+  console.log(username, room);
+  socket.emit('joinRoom', { username, room });
+}
 
-// get username from url
-const {username, room} = Qs.parse(location.search, {
-    ignoreQueryPrefix: true 
+// Get room and users
+socket.on('roomUsers', ({ room, users }) => {
+  outputRoomName(room);
+  outputUsers(users);
 });
 
-
-// Join Room
-socket.emit("joinroom", {username, room});
-
 // Message from server
-socket.on("message", message=>{
-    console.group(message);
-    outputMessage(message);
+socket.on('message', message => {
+  console.log(message);
+  outputMessage(message);
 
-    // scroll down
-    chatMessages.scrollTop = chatMessages.scrollHeight
-})
+  // Scroll down
+  chatMessages.scrollTop = chatMessages.scrollHeight;
+});
 
+// Message submit
+chatForm.addEventListener('submit', e => {
+  e.preventDefault();
 
-// submit
-chatForm.addEventListener('submit', (e)=>{
-    e.preventDefault();
+  // Get message text
+  const msg = e.target.elements.msg.value;
 
-    // Get Message from user
-    const msg = e.target.elements.msg.value;
+  // Emit message to server
+  socket.emit('chatMessage', msg);
 
-    // send message to server
-    socket.emit("chatMessage", msg)
+  // Clear input
+  e.target.elements.msg.value = '';
+  e.target.elements.msg.focus();
+});
 
+// Output message to DOM
+function outputMessage(message) {
+  const div = document.createElement('div');
+  div.classList.add('message');
+  div.innerHTML = `<p class="meta">${message.username} <span>${message.time}</span></p>
+  <p class="text">
+    ${message.text}
+  </p>`;
+  document.querySelector('.chat-messages').appendChild(div);
+}
 
-    // clear input
-    e.target.elements.msg.value = ""
-    e.target.elements.msg.foucs();    
-})
+// Add room name to DOM
+function outputRoomName(room) {
+  roomName.innerText = room;
+}
 
-// Output Message to DOM
-
-function outputMessage(message){
-    const div = document.createElement('div');
-    div.classList.add("message");
-    div.innerHTML =`<p class="meta">${message.userName}<span>${message.time}</span></p>
-    <p class="text">
-        ${message.text}
-    </p>`;
-    document.querySelector(".chat-messages").appendChild(div)
+// Add users to DOM
+function outputUsers(users) {
+  userList.innerHTML = `
+    ${users.map(user => `<li>${user.username}</li>`).join('')}
+  `;
 }

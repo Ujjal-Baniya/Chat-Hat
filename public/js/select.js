16c92b70c3b1491426e7ec76ea489b0b66cc55c0
Ujjal-Baniya <63017383+Ujjal-Baniya@users.noreply.github.com>
2020-08-30 22:33:07 +0530

    Update select.js

--- public/js/select.js
+++ public/js/select.js
@@ -2,14 +2,16 @@ var option = document.getElementsByClassName("option")
 var roomdes = document.getElementById("roomdes");
 const roomid = Math.random(0).toString(36).substr(2,6);
 
+
+// Adding/removing host button after private
 option[1].addEventListener('change', ()=>{
     var host = document.getElementById("later");
     var label1 = document.createElement("label");
     var label2 = document.createElement("label");
     label1.className = "yes option";
     label2.className = "yes option" ;
-    label1.innerHTML = `Host<input type="radio" name="check" required> <i class="fa fa-check" aria-hidden="true"></i>`
-    label2.innerHTML = `<input type="radio" name="check" required> <i class="fa fa-times" aria-hidden="true"></i>`
+    label1.innerHTML = `Host<input type="radio" name="check" value=${roomid} required> <i class="fa fa-check" aria-hidden="true"></i>`
+    label2.innerHTML = `<input type="radio" name="check" value="no" required> <i class="fa fa-times" aria-hidden="true"></i>`
     host.appendChild(label1);
     host.appendChild(label2);
     var yes = document.getElementsByClassName("yes")
@@ -20,8 +22,6 @@ option[1].addEventListener('change', ()=>{
          margin-top: 10%;
          cursor:pointer;
     ">${roomid}<strong>
-        name="RoomID"
-        value="${roomid}"
      </span>`
     });
 
@@ -29,6 +29,8 @@ option[1].addEventListener('change', ()=>{
         roomdes.innerHTML = `<input class="inps"type="text"name="RoomID"id="roomid"placeholder="Room ID"required/>`
     });
 })
+
+// public features
 
 option[0].addEventListener('change', ()=>{
     var host = document.getElementById("later");
